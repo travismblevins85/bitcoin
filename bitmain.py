8395e058c4b3c9a61cdb8bb4c9f280ce2255093e
Travis Blevins <travismblevins85@gmail.com>
2022-03-14 22:53:01 -0400

    3/14/22

--- bitmain.py
+++ bitmain.py
@@ -1,19 +1,25 @@
-import webbrowser
-from datetime import datetime, timedelta, date
+from datetime import date, timedelta
 import pandas as pd
 import plotly.graph_objects as go
 import pandas_datareader as pdr
 
-print("This project is to show you Bitcoin's influence over the rest of the crypto market, the majority of the market follows \nthe same trend, up or down.  I involved 3 or the main coins, Bitcoin, Ethereum (the top 2), and Litecoin.")
-print("To display this, i have two graphs to show you the last 30 days trend, and an input/output to show the current price \ndifference in the 3 of the main coins in the last 24 hours.")
-print("This is really just the beginning of this project, of many things to come, but many updates in the near future. .")
+btc_inf = ("Bitcoin's Influence")
+bt = btc_inf.center(120, '-')
+print(bt)
+print("-This project is to show you Bitcoin's influence over the rest of the crypto market, the majority of the market follows \nthe same trend, up or down.  I involved 3 of the main coins, Bitcoin, Ethereum (the top 2), and Litecoin.")
+print ("\n-To display this, I have a graph to show you the last last 365 days of price trendlines.  I also have have a current\n24 hour percentage change of each coin, which  most likely will be similar.  Many, many updates in the future :)")
+print("\n-Relevance being, is Litecoin is closely related to Bitcoin, but Ethereum is Bitcoin's #1 competitor.")
 
+names = ("Bitcoin, Ethereum, & Litecoin")
+n=names.center(120,'-')
+print(n)
+print("-Bitcoin is a decentralized cryptocurrency originally described in a 2008 whitepaper by a person, or group of people, using the alias Satoshi Nakamoto. It was launched soon after, in January 2009.  Bitcoin is a peer-to-peer online currency, meaning that all transactions happen directly between equal, independent network participants, without the need for any intermediary to permit or facilitate them. Bitcoin was created, according to Nakamoto’s own words, to allow \"online payments to be sent directly from one party to another without going through a financial institution.\"Some concepts for a similar type of a decentralized electronic currency precede BTC, but Bitcoin holds the distinction of being the first-ever cryptocurrency to come into actual use.")
+print("\n-Ethereum is a decentralized open-source blockchain system that features its own cryptocurrency, Ether. ETH works as a platform for numerous other cryptocurrencies, as well as for the execution of decentralized smart contracts.  Ethereum was first described in a 2013 whitepaper by Vitalik Buterin. Buterin, along with other co-founders, secured funding for the project in an online public crowd sale in the summer of 2014. The project team managed to raise $18.3 million in Bitcoin, and Ethereum’s price in the Initial Coin Offering (ICO) was $0.311, with over 60 million Ether sold. Taking Ethereum’s price now, this puts the return on investment (ROI) at an annualized rate of over 270%, essentially almost quadrupling your investment every year since the summer of 2014.  The Ethereum Foundation officially launched the blockchain on July 30, 2015, under the prototype codenamed “Frontier.” Since then, there has been several network updates — “Constantinople” on Feb. 28, 2019, “Istanbul” on Dec. 8, 2019, “Muir Glacier” on Jan. 2, 2020, “Berlin” on April 14, 2021, and most recently on Aug. 5, 2021, the “London” hard fork.  Ethereum’s own purported goal is to become a global platform for decentralized applications, allowing users from all over the world to write and run software that is resistant to censorship, downtime and fraud.")
+print("\n-Litecoin (LTC) is a cryptocurrency that was designed to provide fast, secure and low-cost payments by leveraging the unique properties of blockchain technology.  To learn more about this project, check out our deep dive of Litecoin.  The cryptocurrency was created based on the Bitcoin (BTC) protocol, but it differs in terms of the hashing algorithm used, hard cap, block transaction times and a few other factors. Litecoin has a block time of just 2.5 minutes and extremely low transaction fees, making it suitable for micro-transactions and point-of-sale payments.  Litecoin was released via an open-source client on GitHub on Oct. 7, 2011, and the Litecoin Network went live five days later on Oct. 13, 2011. Since then, it has exploded in both usage and acceptance among merchants and has counted among the top ten cryptocurrencies by market capitalization for most of its existence.  The cryptocurrency was created by Charlie Lee, a former Google employee, who intended Litecoin to be a \"lite version of Bitcoin,\" in that it features many of the same properties as Bitcoin—albeit lighter in weight.")
 
-print("Would you like to see the first graph or skip to the next?  \nYes - shows the graph\nSkip - continues to the next graph")
-if input == ("YES"):
-    get_url= webbrowser.open("https://finance.yahoo.com/chart/BTC-USD#eyJpbnRlcnZhbCI6ImRheSIsInBlcmlvZGljaXR5IjoxLCJjYW5kbGVXaWR0aCI6MzQuOTM3NSwiZmxpcHBlZCI6ZmFsc2UsInZvbHVtZVVuZGVybGF5Ijp0cnVlLCJhZGoiOnRydWUsImNyb3NzaGFpciI6dHJ1ZSwiY2hhcnRUeXBlIjoibGluZSIsImV4dGVuZGVkIjpmYWxzZSwibWFya2V0U2Vzc2lvbnMiOnt9LCJhZ2dyZWdhdGlvblR5cGUiOiJvaGxjIiwiY2hhcnRTY2FsZSI6InBlcmNlbnQiLCJwYW5lbHMiOnsiY2hhcnQiOnsicGVyY2VudCI6MSwiZGlzcGxheSI6IkJUQy1VU0QiLCJjaGFydE5hbWUiOiJjaGFydCIsImluZGV4IjowLCJ5QXhpcyI6eyJuYW1lIjoiY2hhcnQiLCJwb3NpdGlvbiI6bnVsbH0sInlheGlzTEhTIjpbXSwieWF4aXNSSFMiOlsiY2hhcnQiLCLigIx2b2wgdW5kcuKAjCJdfX0sImxpbmVXaWR0aCI6Miwic3RyaXBlZEJhY2tncm91bmQiOnRydWUsImV2ZW50cyI6dHJ1ZSwiY29sb3IiOiIjMDA4MWYyIiwic3RyaXBlZEJhY2tncm91ZCI6dHJ1ZSwiZXZlbnRNYXAiOnsiY29ycG9yYXRlIjp7ImRpdnMiOnRydWUsInNwbGl0cyI6dHJ1ZX0sInNpZ0RldiI6e319LCJjdXN0b21SYW5nZSI6eyJzdGFydCI6MTY0MTAxMzIwMDAwMCwiZW5kIjoxNjQzNjkxNjAwMDAwfSwic3ltYm9scyI6W3sic3ltYm9sIjoiQlRDLVVTRCIsInN5bWJvbE9iamVjdCI6eyJzeW1ib2wiOiJCVEMtVVNEIiwicXVvdGVUeXBlIjoiQ1JZUFRPQ1VSUkVOQ1kiLCJleGNoYW5nZVRpbWVab25lIjoiVVRDIn0sInBlcmlvZGljaXR5IjoxLCJpbnRlcnZhbCI6ImRheSJ9LHsic3ltYm9sIjoiRVRILVVTRCIsInN5bWJvbE9iamVjdCI6eyJzeW1ib2wiOiJFVEgtVVNEIn0sInBlcmlvZGljaXR5IjoxLCJpbnRlcnZhbCI6ImRheSIsImlkIjoiRVRILVVTRCIsInBhcmFtZXRlcnMiOnsiY29sb3IiOiIjNzJkM2ZmIiwid2lkdGgiOjIsImlzQ29tcGFyaXNvbiI6dHJ1ZSwic2hhcmVZQXhpcyI6dHJ1ZSwiY2hhcnROYW1lIjoiY2hhcnQiLCJzeW1ib2xPYmplY3QiOnsic3ltYm9sIjoiRVRILVVTRCJ9LCJwYW5lbCI6ImNoYXJ0IiwiZmlsbEdhcHMiOmZhbHNlLCJhY3Rpb24iOiJhZGQtc2VyaWVzIiwic3ltYm9sIjoiRVRILVVTRCIsImdhcERpc3BsYXlTdHlsZSI6InRyYW5zcGFyZW50IiwibmFtZSI6IkVUSC1VU0QiLCJvdmVyQ2hhcnQiOnRydWUsInVzZUNoYXJ0TGVnZW5kIjp0cnVlLCJoZWlnaHRQZXJjZW50YWdlIjowLjcsIm9wYWNpdHkiOjEsImhpZ2hsaWdodGFibGUiOnRydWUsInR5cGUiOiJsaW5lIiwic3R5bGUiOiJzdHhfbGluZV9jaGFydCIsImhpZ2hsaWdodCI6ZmFsc2V9fSx7InN5bWJvbCI6IkxUQy1VU0QiLCJzeW1ib2xPYmplY3QiOnsic3ltYm9sIjoiTFRDLVVTRCJ9LCJwZXJpb2RpY2l0eSI6MSwiaW50ZXJ2YWwiOiJkYXkiLCJpZCI6IkxUQy1VU0QiLCJwYXJhbWV0ZXJzIjp7ImNvbG9yIjoiI2FkNmVmZiIsIndpZHRoIjoyLCJpc0NvbXBhcmlzb24iOnRydWUsInNoYXJlWUF4aXMiOnRydWUsImNoYXJ0TmFtZSI6ImNoYXJ0Iiwic3ltYm9sT2JqZWN0Ijp7InN5bWJvbCI6IkxUQy1VU0QifSwicGFuZWwiOiJjaGFydCIsImZpbGxHYXBzIjpmYWxzZSwiYWN0aW9uIjoiYWRkLXNlcmllcyIsInN5bWJvbCI6IkxUQy1VU0QiLCJnYXBEaXNwbGF5U3R5bGUiOiJ0cmFuc3BhcmVudCIsIm5hbWUiOiJMVEMtVVNEIiwib3ZlckNoYXJ0Ijp0cnVlLCJ1c2VDaGFydExlZ2VuZCI6dHJ1ZSwiaGVpZ2h0UGVyY2VudGFnZSI6MC43LCJvcGFjaXR5IjoxLCJoaWdobGlnaHRhYmxlIjp0cnVlLCJ0eXBlIjoibGluZSIsInN0eWxlIjoic3R4X2xpbmVfY2hhcnQiLCJoaWdobGlnaHQiOmZhbHNlfX1dLCJzdHVkaWVzIjp7IuKAjHZvbCB1bmRy4oCMIjp7InR5cGUiOiJ2b2wgdW5kciIsImlucHV0cyI6eyJpZCI6IuKAjHZvbCB1bmRy4oCMIiwiZGlzcGxheSI6IuKAjHZvbCB1bmRy4oCMIn0sIm91dHB1dHMiOnsiVXAgVm9sdW1lIjoiIzAwYjA2MSIsIkRvd24gVm9sdW1lIjoiI2ZmMzMzYSJ9LCJwYW5lbCI6ImNoYXJ0IiwicGFyYW1ldGVycyI6eyJ3aWR0aEZhY3RvciI6MC40NSwiY2hhcnROYW1lIjoiY2hhcnQiLCJwYW5lbE5hbWUiOiJjaGFydCJ9fX0sInJhbmdlIjp7ImR0TGVmdCI6IjIwMjEtMTItMzFUMDU6MDA6MDAuMjMxWiIsImR0UmlnaHQiOiIyMDIyLTAyLTAyVDA0OjU5OjAwLjIzMVoiLCJwZXJpb2RpY2l0eSI6eyJpbnRlcnZhbCI6ImRheSIsInBlcmlvZCI6MX0sInBhZGRpbmciOjB9fQ--")
-    open()
-
+graph = ("Trendline Graph")
+trend = graph.center(120,'-')
+print(trend)
 
 CRYPTOS = ['BTC', 'ETH', 'LTC',]
 CURRENCY = 'USD'
@@ -23,7 +29,7 @@ def getData(cryptocurrency):
     #Keep it current with today whenever used
     today = now.strftime("%Y-%m-%d")
     #Go back a month
-    month_earlier = (now - timedelta(days=30)).strftime("%Y-%m-%d")
+    month_earlier = (now - timedelta(days=365)).strftime("%Y-%m-%d")
 
     start = pd.to_datetime(month_earlier)
     end = pd.to_datetime(today)
@@ -31,7 +37,6 @@ def getData(cryptocurrency):
     data = pdr.get_data_yahoo(f'{cryptocurrency}-{CURRENCY}', start, end)
 
     return data
-
 
 #Use dict comprehension which at first is a little confusing to me
 #but key:value for variable in iterable
@@ -52,16 +57,19 @@ for idx, name in enumerate(crypto_data):
         )
 # Center the top title with title_x, or center
 fig.update_layout(
-        title ='The influence of Bitcoin over other cryptos, notice the similar trend lines regardless to price differential.',
+        title ="Bitcoin's influence over other cryptos,notice the similar trendlines over a year.",
         title_x=0.5,
         xaxis_title = 'Date',
         yaxis_title = f'Closing price ({CURRENCY})',
-        legend_title = 'Cryptos'
+          legend_title = 'Cryptos'
     )
 fig.update_yaxes(type='log', tickprefix='$')
 
 fig.show()
 
+per = ("Daily Percentage Change")
+cent = per.center(120,'-')
+print(cent)
 
 print("""Last but not least, I have a small Yahoo! output, showing the change in price within the last 24 hours.  If my theory is correct\n
 then the price differences should be around the same.  Let's find out.""")
@@ -71,10 +79,9 @@ eth = pdr.get_data_yahoo('ETH-USD',start=date.today() - timedelta(hours=24), end
 ltc = pdr.get_data_yahoo('LTC-USD',start=date.today() - timedelta(hours=24), end = date.today())
 
 
-
-x = ((btc["Adj Close"])[1] - (btc["Adj Close"])[0]) / (btc["Adj Close"])[0] * 100
-y = ((eth["Adj Close"])[1] - (eth["Adj Close"])[0]) / (eth["Adj Close"])[0] * 100
-z = ((ltc["Adj Close"])[1] - (ltc["Adj Close"])[0]) / (ltc["Adj Close"])[0] * 100
+x = ((btc["Adj Close"])[1]-(btc["Adj Close"])[0]) / (btc["Adj Close"])[0] * 100
+y = ((eth["Adj Close"])[1]-(eth["Adj Close"])[0]) / (eth["Adj Close"])[0] * 100
+z = ((ltc["Adj Close"])[1]-(ltc["Adj Close"])[0]) / (ltc["Adj Close"])[0] * 100
 print("Bitcoin's daily percentage change is : ", x)
 print("Ethereum's daily percentage change is : ", y)
 print("Litecoins's daily percentage change is : ", z)
